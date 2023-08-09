c532d5262178a0654f4fccf49ddfc925c0d4e657
Rafael Menezes <rafa.rcm@hotmail.com>
2023-08-09 10:21:56 -0300

    Recebendo dados de um filme

---
+++ FimesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using FimesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace FimesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody]Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
