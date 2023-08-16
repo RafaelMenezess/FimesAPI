07ed66138a1f437dd765d835f2b63fa125eae46f
Rafael Menezes <rafa.rcm@hotmail.com>
2023-08-15 21:33:45 -0300

    Conectando ao BD

---
+++ FimesAPI/Data/FilmeContext.cs
@@ -0,0 +1,14 @@
+﻿using FimesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FimesAPI.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> opt) : base(opt)
+        {
+
+        }
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
