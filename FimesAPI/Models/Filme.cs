7169349858d6da205d3f7ed3a2b3e990ca4ab52a
Rafael Menezes <rafa.rcm@hotmail.com>
2023-08-15 21:45:14 -0300

    Gerando a primeira migration

--- FimesAPI/Models/Filme.cs
+++ FimesAPI/Models/Filme.cs
@@ -4,6 +4,8 @@ namespace FimesAPI.Models
 {
     public class Filme
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
 
         [Required(ErrorMessage = "O campo título é obrigatório")]
